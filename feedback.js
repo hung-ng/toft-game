9c44e40d7d81b7db28efda8dcbbda49a3b9a3cd6
hung-ng <hungnguyen35800@gmail.com>
2021-01-09 19:37:12 +0700

    moi

--- feedback.js
+++ feedback.js
@@ -1,14 +1,23 @@
-var Array = []
+var Array1 =new Array();
+if(localStorage.allfeedback==undefined){
+    Array1= [];
+} else{
+Array1 = localStorage.allfeedback.split(',');}
+// if(localStorage.allfeedback.length>0){
+//     // console.log(localStorage.allfeedback);
+//     Array1=localStorage.getItem('allfeedback');
+//     console.log(Array1);
+// }
 document.getElementById('info').addEventListener('submit',function(event){
     event.preventDefault();
     // const feedback =[];
     abc= document.getElementById('info').Feedback.value
     // validate(feedback);
-    console.log(Array);
-    Array.push(abc);
-    console.log(Array);
+    // console.log(Array1);
+    Array1.push(abc);
+    // console.log(Array1);
     localStorage.setItem('allfeedback','');
-    localStorage.allfeedback=JSON.stringify(Array);
+    localStorage.allfeedback=JSON.stringify(Array1);
     // localStorage.setItem('allfeedback',feedback);
     // console.log(feedback);
     document.getElementById('info').Feedback.value=""
