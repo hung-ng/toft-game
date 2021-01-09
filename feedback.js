0622004094650fcdbda7933f9dbab581091bd820
hung-ng <hungnguyen35800@gmail.com>
2021-01-09 19:00:34 +0700

    moi

--- feedback.js
+++ feedback.js
@@ -1,6 +1,15 @@
+var Array = []
 document.getElementById('info').addEventListener('submit',function(event){
     event.preventDefault();
-    const feedback = {feedback: document.getElementById('info').Feedback.value,}  
+    // const feedback =[];
+    abc= document.getElementById('info').Feedback.value
     // validate(feedback);
-    console.log(feedback)
+    console.log(Array);
+    Array.push(abc);
+    console.log(Array);
+    localStorage.setItem('allfeedback','');
+    localStorage.allfeedback=JSON.stringify(Array);
+    // localStorage.setItem('allfeedback',feedback);
+    // console.log(feedback);
+    document.getElementById('info').Feedback.value=""
 })
