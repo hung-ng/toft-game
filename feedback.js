7825c5258a4dab0eda9a35779767c18535610f2e
hung-ng <hungnguyen35800@gmail.com>
2021-01-09 20:11:07 +0700

    moi

--- feedback.js
+++ feedback.js
@@ -1,24 +1,27 @@
-var Array1 =new Array();
-if(localStorage.allfeedback==undefined){
-    Array1= [];
-} else{
-Array1 = localStorage.allfeedback.split(',');}
+var Array1 =[];
+console.log(typeof(Array1))
+if (localStorage.allfeedback == undefined) {
+    Array1 = [];
+} else {
+    console.log(localStorage.allfeedback);
+    Array1 =JSON.parse(localStorage.allfeedback);
+}
 // if(localStorage.allfeedback.length>0){
 //     // console.log(localStorage.allfeedback);
 //     Array1=localStorage.getItem('allfeedback');
 //     console.log(Array1);
 // }
-document.getElementById('info').addEventListener('submit',function(event){
+document.getElementById('info').addEventListener('submit', function (event) {
     event.preventDefault();
     // const feedback =[];
-    abc= document.getElementById('info').Feedback.value
+    abc = document.getElementById('info').Feedback.value
     // validate(feedback);
     // console.log(Array1);
     Array1.push(abc);
     // console.log(Array1);
-    localStorage.setItem('allfeedback','');
-    localStorage.allfeedback=JSON.stringify(Array1);
+    localStorage.setItem('allfeedback', '');
+    localStorage.allfeedback = JSON.stringify(Array1);
     // localStorage.setItem('allfeedback',feedback);
     // console.log(feedback);
-    document.getElementById('info').Feedback.value=""
+    document.getElementById('info').Feedback.value = ""
 })
